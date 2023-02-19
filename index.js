f52eadce730393d2a1dbf8eb1cd7542274d5a6cf
Mohsen Davari <php.mohsen@gmail.com>
2023-02-19 19:01:29 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var http = require('http');
+var nodeList = [
+    "Atlanta.v2ray.online",
+    "Seattle.v2ray.online",
+    "Helsinki.v2ray.online",
+	"Phoenix.v2ray.online",
+	"Vienna.v2ray.online",
+	"Amsterdam.v2ray.online",
+	"LosAngeles.v2ray.online",
+    "Tokyo.v2ray.online"
+];
+
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('سلام');
-    res.end();
+    try {
+        url.hostname = nodeList[Math.floor(Math.random() * nodeList.length)];                        
+        url.protocol = "https";
+      return fetch(new Request(url, req));
+    } catch (e) {
+      return new Response(e);
+    }
+    
 }).listen(process.env.PORT || 3000);
