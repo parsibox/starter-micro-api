b30f3516c29b4311a6cd057796ac6f7fa1d3645a
Mohsen Davari <php.mohsen@gmail.com>
2023-02-19 20:34:31 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
+/*
 var http = require('http');
 const fetch = require('node-fetch');
 
 
-var nodeList = [
-    "Atlanta.v2ray.online",
-    "Seattle.v2ray.online",
-    "Helsinki.v2ray.online",
-	"Phoenix.v2ray.online",
-	"Vienna.v2ray.online",
-	"Amsterdam.v2ray.online",
-	"LosAngeles.v2ray.online",
-    "Tokyo.v2ray.online"
-];
+
 
 http.createServer(function (req, res) {
     console.log(` start at ${req.url}!`);
@@ -31,3 +23,21 @@ http.createServer(function (req, res) {
 
     
 }).listen(process.env.PORT || 3000);
+*/
+var nodeList = [
+    "Atlanta.v2ray.online",
+    "Seattle.v2ray.online",
+    "Helsinki.v2ray.online",
+	"Phoenix.v2ray.online",
+	"Vienna.v2ray.online",
+	"Amsterdam.v2ray.online",
+	"LosAngeles.v2ray.online",
+    "Tokyo.v2ray.online"
+];
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+my_target = 'https://'+nodeList[Math.floor(Math.random() * nodeList.length)]; 
+app.use('*', createProxyMiddleware({target:my_target, changeOrigin: true}));
+app.listen(process.env.PORT || 3000);
+
